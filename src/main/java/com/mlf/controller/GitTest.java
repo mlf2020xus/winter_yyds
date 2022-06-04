2133acca43b4ba1ad5845586cc4b4f0d18c61ed0
tom_glb <goodMorning_glb@mlf.com>
2022-06-04 13:07:27 +0800

    my first commit

--- src/main/java/com/mlf/controller/GitTest.java
+++ src/main/java/com/mlf/controller/GitTest.java
@@ -6,5 +6,6 @@ package com.mlf.controller;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我的分支一");
+        System.out.println("push ssh");
     }
 }
